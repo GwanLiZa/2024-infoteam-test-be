17ded7bc3835f812a720fd293422525d57b4fff7
andongyeon <a56634368@gmail.com>
2024-08-22 19:00:30 +0900

    feat: add auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { AuthController } from './auth.controller';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET_KEY'),
+        signOptions: { expiresIn: '60m' },
+      }),
+      inject: [ConfigService],
+    })
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
